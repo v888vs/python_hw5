27e9198b415a50e57750e232148dfa621223aa43
Valentin Samkov <valentinsamkov@192.168.1.109>
2022-11-09 00:46:15 +0300

    Initial commit

---
+++ hw5.py
@@ -0,0 +1,26 @@
+# Напишите программу, удаляющую из текста
+# все слова, содержащие ""абв"".
+
+# l1 = list(input('Введите символы через пробел: ').split())
+# a1 = 'абв'
+# l2 = [ i for i in l1 if a1 not in i]
+# print(l2)
+
+
+
+# Реализуйте RLE алгоритм: реализуйте
+# модуль сжатия и восстановления данных.
+
+# l1 = input('Введите символы: ')
+# count = 0
+# symb = 0
+# for i in l1:
+#    if symb != i:
+#        if count > 0:
+#            print(count, symb)
+#        symb, count = i, 1
+#    else:
+#        count += 1
+# print(count, symb)
+
+
